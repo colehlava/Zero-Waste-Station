2345d2c433ea1495ef807bf8323574a9db054669
colehlava <35854895+colehlava@users.noreply.github.com>
2018-03-09 13:51:28 -0800

    Add files via upload
    working live pie chart

--- DisplayWeights.py
+++ DisplayWeights.py
@@ -1,5 +1,3 @@
-# DisplayWeights.py
-
 import time
 import serial
 import matplotlib.pyplot as plt
@@ -7,14 +5,12 @@ from matplotlib.animation import FuncAnimation
 
 ser = serial.Serial('/dev/ttyS0',9600,timeout=1)
 
-scale1 = 0
-scale2 = 0
-scale3 = 0
-scale4 = 0
-count = 0
-
 try:
-
+	scale1 = 0
+	scale2 = 0
+	scale3 = 0
+	scale4 = 0
+	count = 0
         labels = ['scale 1', 'scale 2', 'scale 3', 'scale 4']
         sizes = [scale1, scale2, scale3, scale4]
         colors = ['grey', 'b', 'g', 'k']
@@ -26,24 +22,28 @@ try:
                 ax.clear()
                 ax.axis('equal')
                 data = ser.readline() # read data from Arduino
-
+		scale1 = 0
+		scale2 = 0
+		scale3 = 0
+		scale4 = 0
+		count = 0
+		sizes = [scale1, scale2, scale3, scale4]
                 sizes = data.split(',')
                 
-                if count > 0:
-                        scale1 = sizes[0]
-                        scale2 = sizes[1]
-                        scale3 = sizes[2]
-                        scale4 = sizes[3]
-
+		scale1 = sizes[0]
+                scale2 = sizes[1]
+                scale3 = sizes[2]
+                scale4 = sizes[3]
+		
                 scale1 = float(scale1)
                 scale2 = float(scale2)
                 scale3 = float(scale3)
                 scale4 = float(scale4)
 
-				sizes[0] = scale1
-				sizes[1] = scale2
-				sizes[2] = scale3
-				sizes[3] = scale4
+		sizes[0] = scale1
+		sizes[1] = scale2
+		sizes[2] = scale3
+		sizes[3] = scale4
 
                 ax.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
                 count += 1
